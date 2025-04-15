891baa69c73ff6df51abc028ae6146e85c1a36b0
Guanhc416 <121653734+GuanHChen@users.noreply.github.com>
2025-04-15 18:40:33 -0500

    xgboost model hypertuned

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import classification_report, roc_auc_score
 import optuna
 import matplotlib.pyplot as plt
-
+import xgboost as xgb
 
 
 def report(model):
@@ -21,11 +21,12 @@ def report(model):
     print("\nClassification Report:")
     print(classification_report(y_test, y_pred))
 
-def objective(trial):
-    # Optuna hyperparameters
+
+def obj1(trial):
+    # Optuna hyperparameter optimization for forest
     n_estimators = trial.suggest_int('n_estimators', 100, 2000)
     max_depth = trial.suggest_int('max_depth', 10, 50)
-    min_samples_split = trial.suggest_int('min_samples_split', 10, 1000)
+    min_samples_split = trial.suggest_int('min_samples_split', 2, 1000)
     min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 500)
 
     model = RandomForestClassifier(n_estimators=n_estimators,
@@ -36,12 +37,41 @@ def objective(trial):
                                    n_jobs=-1,
                                    random_state=123)
 
-    score = cross_val_score(model, X_val, y_val, cv=5, scoring='recall', n_jobs=-1)
+    score = cross_val_score(model, X_val, y_val, cv=5, scoring='f1', n_jobs=-1)
     return score.mean()
 
-def optimize():
-    study = optuna.create_study(direction='maximize', sampler=optuna.samplers.RandomSampler(seed=123))
-    study.optimize(objective, n_trials=200)
+
+def obj2(trial):
+    # Optuna hyperparameter optimization for XGBoost
+    n_estimators = trial.suggest_int('n_estimators', 100, 2000)
+    eta = trial.suggest_float('eta', 0.01, 0.3)
+    max_depth = trial.suggest_int('max_depth', 3, 50)
+    gamma = trial.suggest_int('gamma', 0, 10)
+    alpha = trial.suggest_int('alpha', 0, 10)
+    reg_lambda = trial.suggest_int('reg_lambda', 0, 10)
+    max_delta_step = trial.suggest_int('max_delta_step', 0, 10)
+    min_child_weight = trial.suggest_int('min_child_weight', 0, 30)
+    scale_pos_weight = 23364 / 6636
+
+    model = xgb.XGBClassifier(n_estimators=n_estimators,
+                              eta=eta,
+                              max_depth=max_depth,
+                              gamma=gamma,
+                              alpha=alpha,
+                              reg_lambda=reg_lambda,
+                              max_delta_step=max_delta_step,
+                              min_child_weight=min_child_weight,
+                              scale_pos_weight=scale_pos_weight,
+                              eval_metric='aucpr',
+                              random_state=123)
+
+    score = cross_val_score(model, X_val, y_val, cv=5, scoring='roc_auc', n_jobs=-1)
+    return score.mean()
+
+
+def optimize(obj):
+    study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler(seed=123))
+    study.optimize(obj, n_trials=200)
     best_params = study.best_params
     print(best_params)
 
@@ -55,7 +85,7 @@ new_cols = ['lim', 'sex', 'edu', 'mar', 'age', 'repay1', 'repay2', 'repay3', 're
 
 dd.columns = new_cols
 
-dd["sex"] = dd["sex"].replace({2:0})
+dd["sex"] = dd["sex"].replace({2: 0})
 
 mapping = {
     1: 3,  # Graduate school to 3
@@ -64,7 +94,7 @@ mapping = {
     4: 0,  # Others to 0
     5: 0,  # Unknown to 0
     6: 0,  # Unknown to 0
-    0: 0   # For some reason, there are 14 instances of 0. These are also remapped to 0.
+    0: 0  # For some reason, there are 14 instances of 0. These are also remapped to 0.
 }
 
 dd['edu'] = dd['edu'].map(mapping)
@@ -73,7 +103,7 @@ enc = OneHotEncoder(sparse_output=False)
 enc_mar = enc.fit_transform(dd[['mar']])
 enc_col = enc.get_feature_names_out(['mar'])
 mar_df = pd.DataFrame(enc_mar, columns=enc_col)
-mar_df = mar_df.iloc[:, [1,2]]
+mar_df = mar_df.iloc[:, [1, 2]]
 
 # Onehot encoded dataframe
 
@@ -92,29 +122,29 @@ dd['util'] = dd[['bill1', 'bill2', 'bill3', 'bill4', 'bill5', 'bill6']].sum(axis
 # 218/637 Greater than 1 = 34% Statistically Significant!
 # 51/201 Less than 0 = 25% Statistically Insig
 
-repay_cols = ['repay1', 'repay2', 'repay3', 'repay4', 'repay5', 'repay6'] # list of the repayment variables
-dd['late'] = (dd[repay_cols] > 0).sum(axis=1) # count of total late payments
+repay_cols = ['repay1', 'repay2', 'repay3', 'repay4', 'repay5', 'repay6']  # list of the repayment variables
+dd['late'] = (dd[repay_cols] > 0).sum(axis=1)  # count of total late payments
 
-bill_cols = ['bill1','bill2','bill3','bill4','bill5','bill6'] # list of the bill variables (spending)
-dd['vol'] = dd[bill_cols].var(axis=1) # variance in spending
+bill_cols = ['bill1', 'bill2', 'bill3', 'bill4', 'bill5', 'bill6']  # list of the bill variables (spending)
+dd['vol'] = dd[bill_cols].var(axis=1)  # variance in spending
 
 target = dd.pop('default')
-dd['default'] = target # make default the last column
+dd['default'] = target  # make default the last column
 
 X = dd.drop('default', axis=1)
 y = dd['default']
 
-X_train, X_test, y_train, y_test = train_test_split( # 60-20-20 Split for Train, Validation, Test
+X_train, X_test, y_train, y_test = train_test_split(  # 60-20-20 Split for Train, Validation, Test
     X, y, test_size=0.2, random_state=123, stratify=y)
 X_train, X_val, y_train, y_val = train_test_split(
     X_train, y_train, test_size=0.25, random_state=123, stratify=y_train)
 
-scaler = StandardScaler() # initialize scaler
+scaler = StandardScaler()  # initialize scaler
 
-scale_cols = ['lim','age',
-              'bill1','bill2','bill3','bill4','bill5','bill6',
-              'pay1','pay2','pay3','pay4','pay5','pay6',
-              'util','late','vol']
+scale_cols = ['lim', 'age',
+              'bill1', 'bill2', 'bill3', 'bill4', 'bill5', 'bill6',
+              'pay1', 'pay2', 'pay3', 'pay4', 'pay5', 'pay6',
+              'util', 'late', 'vol']
 
 ct = ColumnTransformer(
     [('scaler', StandardScaler(), scale_cols)],
@@ -126,9 +156,7 @@ X_train = ct.fit_transform(X_train)
 X_test = ct.transform(X_test)
 X_val = ct.transform(X_val)
 
-rf = RandomForestClassifier(random_state=123)
-
-rf2 = RandomForestClassifier(
+rf = RandomForestClassifier(
     criterion='entropy',
     max_depth=41,
     min_samples_leaf=2,
@@ -137,13 +165,23 @@ rf2 = RandomForestClassifier(
     random_state=123
 )
 
-#rf2.fit(X_train, y_train)
+#rf.fit(X_train, y_train)
 
-#report(rf2)
+#report(rf)
 
-#optimize()
+bm = xgb.XGBClassifier(objective='binary:logistic',  # Or 'multi:softmax' for multi-class
+                       n_estimators=1359,          # Number of boosting rounds (trees)
+                       eta=0.09796,         # Step size shrinkage to prevent overfitting
+                       max_depth=32,               # Maximum depth of a tree
+                       #gamma=9,
+                       #alpha=8,
+                       #reg_lambda=6,
+                       max_delta_step=1,
+                       min_child_weight=16,
+                       eval_metric='aucpr',
+                       random_state=123)           # For reproducibility
 
+bm.fit(X_train, y_train)
+report(bm)
 
-
-
-
+#optimize(obj2)
